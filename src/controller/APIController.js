56bfcaee0e841c7f9bcd62125f623b0d44618d56
DKVI <92564346+DKVI@users.noreply.github.com>
2023-06-23 23:32:48 +0700

    commit

--- src/controller/APIController.js
+++ src/controller/APIController.js
@@ -6,6 +6,9 @@ let getAllUsers = async (req, res) => {
 
 let createUser = async (req, res) => {
   const { firstName, lastName, email, address } = req.body;
+  if (!firstName || !lastName || !email || !address) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
   await pool.execute(
     `INSERT INTO users(firstName, lastName, email, address) VALUES ( ?, ?, ?, ?)`,
     [firstName, lastName, email, address]
@@ -13,4 +16,25 @@ let createUser = async (req, res) => {
   return res.status(200).json({ message: "Access Create User" });
 };
 
-export { getAllUsers, createUser };
+let updateUser = async (req, res) => {
+  const userId = req.params.id;
+  const { firstName, lastName, email, address } = req.body;
+  if (!firstName || !lastName || !email || !address) {
+    return res.status(400).json({ message: "Missing fields" });
+  }
+  await pool.execute(
+    `UPDATE users
+  SET firstName = ?, lastName = ?, email = ?, address = ?
+  WHERE id = ?;`,
+    [firstName, lastName, email, address, userId]
+  );
+  return res.status(200).json({ message: "update successfully" });
+};
+
+let deleteUser = async (req, res) => {
+  const userId = req.params.id;
+  pool.execute("delete from users where id = ?", [userId]);
+  return res.status(200).json({ message: "delete successfully" });
+};
+
+export { getAllUsers, createUser, updateUser, deleteUser };
--- src/route/api.js
+++ src/route/api.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { getAllUsers, createUser } from "../controller/APIController";
+import {
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controller/APIController";
 
 let router = express.Router();
 
 const initAPI = (app) => {
   router.get("/users", getAllUsers);
   router.post("/create-user", createUser);
+  router.put("/update-user/:id", updateUser);
+  router.delete("/delete-user/:id", deleteUser);
   return app.use("/api/v1/", router);
 };
 
